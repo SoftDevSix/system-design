f6fad4d5fb3fc92fed47e429157549dfbaf42b77
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-10 23:29:36 -0400

    test: added a new unit test to analyze code from jar file

--- libraries-for-ast-analysis/spoon-library/app/src/test/java/org/example/tests_for_processor/CatchProcessorTest.java
+++ libraries-for-ast-analysis/spoon-library/app/src/test/java/org/example/tests_for_processor/CatchProcessorTest.java
@@ -3,16 +3,28 @@ package org.example.tests_for_processor;
 import org.example.processors.CatchProcessor;
 import org.junit.Test;
 
+import spoon.JarLauncher;
 import spoon.Launcher;
+import spoon.decompiler.ProcyonDecompiler;
 
 public class CatchProcessorTest {
-    
-    private final String CLASS_PATH = "src/main/java/org/example/modules_to_analyze/NumberHandler.java";
 
     @Test
     public void verifyEmptyCatchAnalysis() {
+        String classPath = "src/main/java/org/example/modules_to_analyze/NumberHandler.java";
         Launcher launcher = new Launcher();
-        launcher.addInputResource(CLASS_PATH);
+        launcher.addInputResource(classPath);
+        CatchProcessor processor = new CatchProcessor();
+        launcher.addProcessor(processor);
+        launcher.buildModel();
+        launcher.process();
+    }
+
+    @Test
+    public void verifyEmptyCatchAnalysisFromJar() {
+        String jarPath = "src/test/resources/jarprj-1.0-SNAPSHOT.jar";
+        JarLauncher launcher = new JarLauncher(jarPath, null, null, new ProcyonDecompiler());
+        launcher.addInputResource(jarPath);
         CatchProcessor processor = new CatchProcessor();
         launcher.addProcessor(processor);
         launcher.buildModel();
